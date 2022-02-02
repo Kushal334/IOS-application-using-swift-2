ffa8e3b93204d590a6e4b6d06fc492f4d30c65fa
Kushal Shingote <kushalshingote2@gmail.com>
2022-02-02 18:09:56 +0530

    Add files via upload

---
+++ tip_calculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  tip_calculatorTests.swift
+//  tip_calculatorTests
+//
+//  Created by Isaac Atif on 6/18/18.
+//  Copyright © 2018 Isaac Atif. All rights reserved.
+//
+
+import XCTest
+@testable import tip_calculator
+
+class tip_calculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
